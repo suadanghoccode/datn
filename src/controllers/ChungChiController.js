be0c29659a144346d3209c0d5a6b4af5ec7ea03e
suadanghoccode <leilapham1123@gmail.com>
2025-05-08 14:57:02 +0700

    Update code

--- src/controllers/ChungChiController.js
+++ src/controllers/ChungChiController.js
@@ -2,17 +2,37 @@ import db from '../models';
 import ChungChiServices from "../services/ChungChiServices";
 let getAllChungChi = async (req, res) => {
     let data = await ChungChiServices.getAllChungChi();
-    console.log(data);
     return res.render('danhsach_chungchi.ejs', {
         dataTable : data
     });
 };
 let postChungChi = async (req, res) => {
     let message = await ChungChiServices.createNewChungChi(req.body);
-    console.log(message);
+    res.redirect('/danh-sach-chung-chi');
+}
+let getEditChungChi = async (req, res) => {
+    let chungchi_id = req.query.id;
+    if(chungchi_id){
+        let chungchi_data = await ChungChiServices.getChungChiId(chungchi_id);
+        console.log(chungchi_data);
+        return res.send('Tìm thấy chứng chỉ');
+    }else{
+        return res.send('Không tìm thấy chứng chỉ');
+    }
+}
+
+let putChungChi = async(req, res) => {
+    try {
+        let data = req.body;
+        await ChungChiServices.updateChungChiData (data);
+        return res.redirect('/danh-sach-chung-chi'); 
+    } catch (error) {
+        res.send('Lỗi: ' + error.message);
+    }
 }
 module.exports = {
     getAllChungChi : getAllChungChi,
     postChungChi: postChungChi,
-
+    getEditChungChi: getEditChungChi,
+    putChungChi: putChungChi,
 }
--- src/route/web.js
+++ src/route/web.js
@@ -3,8 +3,12 @@ import ChungChiController from '../controllers/ChungChiController';
 const router = express.Router();
 
 const initWebRoutes = (app) => {
+
+    //chứng chỉ ở đâyđây
     router.get('/danh-sach-chung-chi', ChungChiController.getAllChungChi);
     router.post('/post-chungchi', ChungChiController.postChungChi);
+    router.post('/edit-chungchi', ChungChiController.getEditChungChi);
+    router.post('/put-chungchi', ChungChiController.putChungChi);
     app.use("/", router);
 };
 
